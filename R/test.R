b3122b52947eefa95f372d0023c5ae06c3d53073
Simon Müller <info@muon-stat.com>
2015-07-24 23:05:44 +0200

    test

--- R/test.R
+++ R/test.R
@@ -1,40 +1,40 @@
-library(survival)
-library(cbr)
-
-# Cox model
-ovarian$tt <- factor(sample(1:3, 26, replace=T))
-ovarian$resid.ds <- factor(ovarian$resid.ds)
-ovarian$rx <- factor(ovarian$rx)
-ovarian$ecog.ps <- factor(ovarian$ecog.ps)
-
-sc <- cbrCoxModel$new(learning=ovarian, newData=ovarian, learnVars=names(ovarian)[-c(1, 2)], endPoint=c("futime", "fustat"))
-sc$learn()
-sc$getSimilarCases(1)
-sc$simCases
-pp <- sc$validate()
-pp <- sc$check_linearity()
-
-sc$getFullDistanceMatrix()
-p <- cmdscale(sc$distMat, k = 2)
-plot(p, col=as.factor(ovarian$fustat))
-p <- prcomp(sc$distMat)
-plot(p, col=as.factor(ovarian$rx))
-library(Rtsne)
-p <- Rtsne(sc$distMat, is_distance = T, perplexity = 5, theta = .1)
-plot(p$Y, col=as.factor(ovarian$rx))
-
-
-# RF model
-ovarian$tt <- NULL
-ovarian$rx <- factor(ovarian$rx)
-sc <- cbrRFProxy$new(learning=ovarian, newData=ovarian, learnVars=names(ovarian)[-c(1, 2)], endPoint=c("futime", "fustat"), impute=TRUE)
-sc$learn()
-sc$getSimilarCases(nCases=1)
-pp <- sc$validate()
-
-sc$getFullDistanceMatrix()
-res <- Rtsne(X = sc$distMat, dims=2, perplexity = 5, is_distance = T)
-plot(res$Y, col=as.factor(ovarian$ecog.ps))
-
-ovarian$age[3] <- NA
-rf <- rfsrc(Surv(futime, fustat) ~ age + resid.ds + rx, data=ovarian, na.action = "na.impute")
+# library(survival)
+# library(cbr)
+# 
+# # Cox model
+# ovarian$tt <- factor(sample(1:3, 26, replace=T))
+# ovarian$resid.ds <- factor(ovarian$resid.ds)
+# ovarian$rx <- factor(ovarian$rx)
+# ovarian$ecog.ps <- factor(ovarian$ecog.ps)
+# 
+# sc <- cbrCoxModel$new(learning=ovarian, newData=ovarian, learnVars=names(ovarian)[-c(1, 2)], endPoint=c("futime", "fustat"))
+# sc$learn()
+# sc$getSimilarCases(1)
+# sc$simCases
+# pp <- sc$validate()
+# pp <- sc$check_linearity()
+# 
+# sc$getFullDistanceMatrix()
+# p <- cmdscale(sc$distMat, k = 2)
+# plot(p, col=as.factor(ovarian$fustat))
+# p <- prcomp(sc$distMat)
+# plot(p, col=as.factor(ovarian$rx))
+# library(Rtsne)
+# p <- Rtsne(sc$distMat, is_distance = T, perplexity = 5, theta = .1)
+# plot(p$Y, col=as.factor(ovarian$rx))
+# 
+# 
+# # RF model
+# ovarian$tt <- NULL
+# ovarian$rx <- factor(ovarian$rx)
+# sc <- cbrRFProxy$new(learning=ovarian, newData=ovarian, learnVars=names(ovarian)[-c(1, 2)], endPoint=c("futime", "fustat"), impute=TRUE)
+# sc$learn()
+# sc$getSimilarCases(nCases=1)
+# pp <- sc$validate()
+# 
+# sc$getFullDistanceMatrix()
+# res <- Rtsne(X = sc$distMat, dims=2, perplexity = 5, is_distance = T)
+# plot(res$Y, col=as.factor(ovarian$ecog.ps))
+# 
+# ovarian$age[3] <- NA
+# rf <- rfsrc(Surv(futime, fustat) ~ age + resid.ds + rx, data=ovarian, na.action = "na.impute")
