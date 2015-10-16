11eeb84f4e2a5d7085f26dd45c16e57011393985
Simon Müller <info@muon-stat.com>
2015-10-17 00:23:23 +0200

    add doc for distance measures

---
+++ distance-measures.R
@@ -0,0 +1,19 @@
+# Distance Measures used in this Package
+
+TODO: Motivation
+
+## Similarity Measure
+
+df
+
+### Cox-Beta
+
+dsfs
+
+### RandomForest Proximity
+
+dsf
+
+### RandomForest Deep Model
+
+sdf
