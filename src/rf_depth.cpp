85a9eaba86999d6f9a795ccc60eab4d4a54b68e5
Simon Müller <info@muon-stat.com>
2016-09-05 22:37:21 +0200

    hash

--- src/rf_depth.cpp
+++ src/rf_depth.cpp
@@ -35,7 +35,7 @@ typedef tbb::concurrent_unordered_map<std::pair <int, int>, std::vector<double> 
 // custom hash function for pair
 namespace std {
   template <>
-  struct std::hash<std::pair<std::uint32_t, std::uint32_t> > {
+  struct hash<std::pair<std::uint32_t, std::uint32_t> > {
     inline std::uint64_t operator()(std::pair<uint32_t, uint32_t>& k) const {
       //should produce no collisions
       //http://stackoverflow.com/a/24693169/1069256
