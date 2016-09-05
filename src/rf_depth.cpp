4f4eeee18aa22b71e81980eb6a66a19411553bd0
Simon Müller <info@muon-stat.com>
2016-09-05 22:33:13 +0200

    update hash

--- src/rf_depth.cpp
+++ src/rf_depth.cpp
@@ -35,13 +35,13 @@ typedef tbb::concurrent_unordered_map<std::pair <int, int>, std::vector<double> 
 // custom hash function for pair
 namespace std {
   template <>
-  struct hash<std::pair<uint32_t, uint32_t> > {
-    inline uint64_t operator()(std::pair<uint32_t, uint32_t>& k) const {
+  struct std::hash<std::pair<std::uint32_t, std::uint32_t> > {
+    inline std::uint64_t operator()(std::pair<uint32_t, uint32_t>& k) const {
       //should produce no collisions
       //http://stackoverflow.com/a/24693169/1069256
       //return f << (CHAR_BIT * sizeof(size_t) / 2) | s;
       //http://stackoverflow.com/questions/2768890/how-to-combine-two-32-bit-integers-into-one-64-bit-integer?lq=1
-      return (uint64_t) k.first << 32 | k.second;
+      return (std::uint64_t) k.first << 32 | k.second;
     }
   };
 }
