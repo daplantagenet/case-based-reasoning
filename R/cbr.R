c7c4e6aa01de02a2cebf50cb2082ddea16c0e5bc
Simon Müller <info@muon-stat.com>
2016-09-18 23:22:32 +0200

    clean from cpp code

--- R/cbr.R
+++ R/cbr.R
@@ -1,10 +1,6 @@
 #' case based reasoning
 #' @docType package
 #' @name cbr
-#' @import Rcpp
-#' @import RcppArmadillo
 #' @import R6
 #' @import data.table
-#' @importFrom RcppParallel RcppParallelLibs
-#' @useDynLib cbr
 NULL
